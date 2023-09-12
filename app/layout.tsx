6562893e0c6757b71d7ca721cec7258672fde4a1
Raoul Federico <federico.raoul@gmail.com>
2023-09-12 21:03:31 +0200

    aggiunta navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ThemeProvider } from "@/components/theme-provider"
+import Navbar from '@/components/header/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,8 +20,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <Navbar/>
             {children}
-          </ThemeProvider>
+        </ThemeProvider>
       </body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1>PROVA GIT seconda prova</h1>
-      </div>
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <h1>Ciao</h1>
     </main>
   )
 }
